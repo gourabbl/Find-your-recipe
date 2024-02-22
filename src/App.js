c9403b6f531530be9a3d5b95667fbd8dddc481b6
Gourab Bal <133105564+gourabbl@users.noreply.github.com>
2024-02-23 00:29:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,57 +1,203 @@
-import { useState, useEffect } from 'react';
-import Recipe from './Recipe.js';
+import React, { useState } from "react";
+import Axios from "axios";
+import styled from "styled-components";
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogActions from "@material-ui/core/DialogActions";
 
+const APP_ID = "a52b4d43";
+const APP_KEY = "e0e5c667605f5e91d8275c973531b80a";
 
+const RecipeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  width: 300px;
+  box-shadow: 0 3px 10px 0 #aaa;
+`;
+const CoverImage = styled.img`
+  object-fit: cover;
+  height: 200px;
+`;
+const RecipeName = styled.span`
+  font-size: 18px;
+  font-weight: 600;
+  color: black;
+  margin: 10px 0;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+const SeeMoreText = styled.span`
+  color: #eb3300;
+  font-size: 18px;
+  text-align: center;
+  border: solid 1px #eb3300;
+  border-radius: 3px;
+  padding: 10px 15px;
+  cursor: pointer;
+`;
+const IngredientsText = styled(SeeMoreText)`
+  color: green;
+  border: solid 1px green;
+  margin-bottom: 12px;
+`;
+const SeeNewTab = styled(SeeMoreText)`
+  color: green;
+  border: solid 1px green;
+`;
+const RecipeComponent = (props) => {
+  const [show, setShow] = useState("");
 
-export default function App() {
+  const { label, image, ingredients, url } = props.recipe;
+  return (
+    <RecipeContainer>
+      <Dialog
+        onClose={() => console.log("adsadad")}
+        aria-labelledby="simple-dialog-title"
+        open={!!show}
+      >
+        <DialogTitle>Ingredients</DialogTitle>
+        <DialogContent>
+          <RecipeName>{label}</RecipeName>
+          <table>
+            <thead>
+              <th>Ingredient</th>
+              <th>Weight</th>
+            </thead>
+            <tbody>
+              {ingredients.map((ingredient, index) => (
+                <tr key={index} className="ingredient-list">
+                  <td>{ingredient.text}</td>
+                  <td>{ingredient.weight}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </DialogContent>
+        <DialogActions>
+          <SeeNewTab onClick={() => window.open(url)}>See More</SeeNewTab>
+          <SeeMoreText onClick={() => setShow("")}>Close</SeeMoreText>
+        </DialogActions>
+      </Dialog>
+      <CoverImage src={image} alt={label} />
+      <RecipeName>{label}</RecipeName>
+      <IngredientsText onClick={() => setShow(!show)}>
+        Ingredients
+      </IngredientsText>
+      <SeeMoreText onClick={() => window.open(url)}>
+        See Complete Recipe
+      </SeeMoreText>
+    </RecipeContainer>
+  );
+};
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const AppName = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+const Header = styled.div`
+  background-color: black;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
+  padding: 20px;
+  font-size: 25px;
+  font-weight: bold;
+  box-shadow: 0 3px 6px 0 #555;
+`;
+const SearchBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 10px 10px;
+  border-radius: 6px;
+  margin-left: 20px;
+  width: 50%;
+  background-color: white;
+`;
+const SearchIcon = styled.img`
+  width: 32px;
+  height: 32px;
+`;
+const RecipeImage = styled.img`
+  width: 36px;
+  height: 36px;
+  margin: 15px;
+`;
+const Placeholder = styled.img`
+  width: 120px;
+  height: 120px;
+  margin: 200px;
+  opacity: 50%;
+`;
+const SearchInput = styled.input`
+  color: black;
+  font-size: 16px;
+  font-weight: bold;
+  border: none;
+  outline: none;
+  margin-left: 15px;
+`;
+const RecipeListContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding: 30px;
+  gap: 20px;
+  justify-content: space-evenly;
+`;
+const AppComponent = () => {
+  const [searchQuery, updateSearchQuery] = useState("");
+  const [recipeList, updateRecipeList] = useState([]);
+  const [timeoutId, updateTimeoutId] = useState();
+  const fetchData = async (searchString) => {
+    const response = await Axios.get(
+      `https://api.edamam.com/search?q=${searchString}&app_id=${APP_ID}&app_key=${APP_KEY}`,
+    );
+    updateRecipeList(response.data.hits);
+  };
 
-  const [saveItem, setSaveItem] = useState([])  //storing API
-  const [search, setSearch] = useState("") //storing search items
-  const [finalSubmit, setFinalSubmit] = useState("") //after pressing submit
-
-  useEffect(()=>{
-    getRecipe()
-  },[finalSubmit])
-
-  const getRecipe = async ()=>{
-    const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${finalSubmit}&app_id=52324e2f&app_key=29bcb5df6f313ae562adb2c23e1c5f63`);
-    const data = await response.json();
-    setSaveItem(data.hits)
-    console.log(data.hits)
-  }
-
-  const searchItem = (event) =>{
-    setSearch(event.target.value)
-  }
-
-  const submit = (event)=>{
-    event.preventDefault()
-    setFinalSubmit(search)
-  }
-  
+  const onTextChange = (e) => {
+    clearTimeout(timeoutId);
+    updateSearchQuery(e.target.value);
+    const timeout = setTimeout(() => fetchData(e.target.value), 500);
+    updateTimeoutId(timeout);
+  };
 
   return (
-    <div>
-      <img className='cover-image' src='../cover image.jpg' />
-      <div>
-      <img className='logo' src='../Main--logo.svg' />
-      <h1 className='logo--title'>Cooked</h1>
-      </div>
-     
-    <form className='input--search' onSubmit={submit}>
-       <input type='text' placeholder='Search for recipe' onChange={searchItem} />
-       <button>Search</button> 
-    </form>
-    <div>
-    {saveItem.map(recipes =>(
-      <Recipe 
-        image={recipes.recipe.image}
-        title={recipes.recipe.label}
-        calories={recipes.recipe.calories}
-      />
-    ))}
-    </div>
-    
-    </div>
-  )
-  };
+    <Container>
+      <Header>
+        <AppName>
+          <RecipeImage src="/react-recipe-finder/hamburger.svg" />
+          Recipe Finder
+        </AppName>
+        <SearchBox>
+          <SearchIcon src="/react-recipe-finder/search-icon.svg" />
+          <SearchInput
+            placeholder="Search Recipe"
+            value={searchQuery}
+            onChange={onTextChange}
+          />
+        </SearchBox>
+      </Header>
+      <RecipeListContainer>
+        {recipeList?.length ? (
+          recipeList.map((recipe, index) => (
+            <RecipeComponent key={index} recipe={recipe.recipe} />
+          ))
+        ) : (
+          <Placeholder src="/react-recipe-finder/hamburger.svg" />
+        )}
+      </RecipeListContainer>
+    </Container>
+  );
+};
+
+export default AppComponent;
